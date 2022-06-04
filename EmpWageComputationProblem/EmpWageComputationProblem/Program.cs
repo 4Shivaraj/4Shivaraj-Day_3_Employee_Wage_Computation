444952c18d07b9eb56b752cf7fe63cbea7294b27
4Shivaraj <4shivaraj.gowda@gmail.com>
2022-06-05 00:41:50 +0530

    [Shivaraj] Added Employee present or Absent Solution

--- EmpWageComputationProblem/EmpWageComputationProblem/Program.cs
+++ EmpWageComputationProblem/EmpWageComputationProblem/Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Welcome To Employee Wage Computation Problem");
+﻿//Console.WriteLine("Welcome To Employee Wage Computation Problem");
+namespace Program
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //UC1
+            EmpPresentAbsent prg = new EmpPresentAbsent();
+            prg.CheckEmpPresentAbsent();
+        }
+    }
+}
