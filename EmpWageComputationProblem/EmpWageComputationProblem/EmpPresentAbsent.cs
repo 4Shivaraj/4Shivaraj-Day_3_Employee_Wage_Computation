fade35b64479fa13b6cafc25fef05d80857fbdfa
4Shivaraj <4shivaraj.gowda@gmail.com>
2022-06-10 22:50:18 +0530

    [Shivaraj] Added Multiple company Problem Solution

--- EmpWageComputationProblem/EmpWageComputationProblem/EmpPresentAbsent.cs
+++ EmpWageComputationProblem/EmpWageComputationProblem/EmpPresentAbsent.cs
@@ -4,37 +4,46 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-// UC - 7
-//Refactor the Code to write a Class Method to Compute Employee Wage - Use Class Method and Class Variables
-// Calculate Wages till a condition of total working hours or days is reached for a month
-// Assume 100 hours
+//UC-8
+//Compute Employee Wage for multiple companies - Note: Each Company has its ownwage, 
+//number of working days and working hours per month 
+//- Use Class Method with function parameters instead of Class Variables
 
-namespace Program
+namespace oopsday2
 {
-    internal class EmployeeWage
+    class EmployeeWageComputation
     {
-        public const float EmpWagePerHour = 20;
-        public const int FullTime_WorkingHrs_PerDay = 8;
-        public const int PartTime_WorkingHours_PerDay = 4;
-        public const int MAX_WORKING_HRS = 100;
-        public const int MAX_WORKING_DAYS = 20;
         public const int IS_FULL_TIME = 1;
         public const int IS_PART_TIME = 2;
         public const int IS_ABSENT = 0;
         float EmpDailyWage = 0;
         public float TotalWage = 0;
+        private Dictionary<String, Company> Companies = new Dictionary<String, Company>();
 
+        public void AddCompany(String CompanyName, int EmpWagePerhour, int FullTime_WorkingHrs_PerDay, int PartTime_WorkingHours_PerDay, int MAX_WORKING_HRS, int MAX_WORKING_DAYS)
+        {
+            Company company = new Company(CompanyName.ToLower(), EmpWagePerhour, FullTime_WorkingHrs_PerDay, PartTime_WorkingHours_PerDay, MAX_WORKING_HRS, MAX_WORKING_DAYS);
+            Companies.Add(CompanyName.ToLower(), company);
+        }
+    
         public int IsEmployeePresent()
         {
             return new Random().Next(0, 3);
         }
 
-        public void CalculateEmpWage()
+        public void CalculateEmpWage(string CompanyName )
         {
             int DayNumber = 1;
             int EmpWorkingHours = 0;
             int TotalWorkingHrs = 0;
-            while (DayNumber <= MAX_WORKING_DAYS && TotalWorkingHrs <= MAX_WORKING_HRS)
+
+            if (!Companies.ContainsKey(CompanyName.ToLower()))
+                  throw new ArgumentNullException("Company Don't Exist");
+            Companies.TryGetValue(CompanyName.ToLower(), out Company company);
+
+
+
+            while (DayNumber <= company.MAX_WORKING_DAYS && TotalWorkingHrs <= company.MAX_WORKING_HRS)
             {
 
                 switch (IsEmployeePresent())
@@ -44,20 +53,20 @@ namespace Program
                         break;
 
                     case IS_PART_TIME:
-                        EmpWorkingHours = PartTime_WorkingHours_PerDay;
+                        EmpWorkingHours = company.PartTime_WorkingHours_PerDay;
                         break;
 
                     case IS_FULL_TIME:
-                        EmpWorkingHours = FullTime_WorkingHrs_PerDay;
+                        EmpWorkingHours = company.FullTime_WorkingHrs_PerDay;
                         break;
                 }
-                EmpDailyWage = EmpWorkingHours * EmpWagePerHour;
+                EmpDailyWage = EmpWorkingHours * company.EmpWagePerHour;
 
                 TotalWage += EmpDailyWage;
                 DayNumber++;
                 TotalWorkingHrs += EmpWorkingHours;
             }
-            Console.WriteLine("Total working days :" + (DayNumber) + "\n Total working hours :" + TotalWorkingHrs + "");
+            Console.WriteLine("Total working days :" + (DayNumber -1 ) + "\n Total working hours :" + TotalWorkingHrs + "");
 
         }
 
--- EmpWageComputationProblem/EmpWageComputationProblem/Program.cs
+++ EmpWageComputationProblem/EmpWageComputationProblem/Program.cs
@@ -1,13 +1,28 @@
 ﻿//Console.WriteLine("Welcome To Employee Wage Computation Problem");
-namespace Program
-{
+namespace oopsday2
+{ 
     class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            //UC6
-            EmployeeWage prg = new EmployeeWage();
-            prg.CalculateEmpWage();
+            
+            Console.WriteLine("1 For TATA Company Employee Wage");
+            Console.WriteLine("2 For MAHINDRA Company Employee Wage");
+            
+            int problem = Convert.ToInt32(Console.ReadLine());
+            switch (problem)
+            {
+                case 1:
+                    EmployeeWageComputation employeewagecomputation = new EmployeeWageComputation();
+                    employeewagecomputation.AddCompany("TATA", 20, 8, 4, 100, 20);
+                    employeewagecomputation.CalculateEmpWage("TATA");
+                    break;
+                case 2:
+                    EmployeeWageComputation wagecomputation = new EmployeeWageComputation();
+                    wagecomputation.AddCompany("MAHINDRA", 30, 8, 4, 100, 20);
+                    wagecomputation.CalculateEmpWage("MAHINDRA");
+                    break;
+            }
         }
     }
 }
