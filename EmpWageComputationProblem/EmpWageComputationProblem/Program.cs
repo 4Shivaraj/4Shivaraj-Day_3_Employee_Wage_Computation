86d226a4066a77dafc63406d9907709108f0ff5b
4Shivaraj <4shivaraj.gowda@gmail.com>
2022-06-17 03:25:06 +0530

    [Shivaraj] For Multiple Company Storing Daily wage along with Total wage

--- EmpWageComputationProblem/EmpWageComputationProblem/Program.cs
+++ EmpWageComputationProblem/EmpWageComputationProblem/Program.cs
@@ -5,7 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 
 //UC-13
-//Store the Daily Wage along with the Total Wage
+//Ability to get the Total Wage when queried by Company
+//- Create Method in EmpWageBuilder to get total wage by Company
 
 namespace EmployeewageComputation
 {
@@ -21,12 +22,26 @@ namespace EmployeewageComputation
             employeeComputation.AddCompany("MAHINDRA", 30, 8, 4, 100, 20);
             employeeComputation.CalculateEmpWage("MAHINDRA");
 
-            employeeComputation.displayArray();
+            //employeeComputation.displayArray();
+
+            Console.WriteLine("Enter the name of the company you want to see the total wage of: ");
+            string Name = Console.ReadLine();
+            employeeComputation.displayArray(Name);
         }
     }
 }
 
 //Result
-//Monthly wage for TATA with Daily wage = 160 is 2080
+//Enter the name of the company you want to see the total wage of:
+//TATA
 
-//Monthly wage for MAHINDRA with Daily wage = 240 is 3000
+//Monthly wage for TATA with
+//Daily wage = 160 is 2080
+
+//------------------------------------------------------------------
+
+//Enter the name of the company you want to see the total wage of:
+//MAHINDRA
+
+//Monthly wage for MAHINDRA with
+//Daily wage = 240 is 3120
